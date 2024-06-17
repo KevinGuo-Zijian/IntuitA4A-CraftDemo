dca2400f4dc6189da96144f24417a614f4baa8c7
KevinGuo-Zijian <kevin.guo.data@gmail.com>
2024-06-17 16:23:15 -0700

    Add files via upload

---
+++ fraudstrategy1/src/main/java/org/example/fraudstrategy1/Decision.java
@@ -0,0 +1,29 @@
+// ORGIN
+package org.example.fraudstrategy1;
+
+
+public class Decision {
+	private String decision;
+	private String ruleName;
+	
+    public Decision() {
+        this.decision = "Approve      ";
+        this.ruleName = "No Rules Triggered - Approve";
+    }
+    
+	public String getDecision() {
+		return decision;
+	}
+// TODO
+	public void setDecision(String decision) {
+		this.decision = decision;
+		
+	}
+    public String getRuleName() {
+        return ruleName;
+    }
+
+    public void setRuleName(String ruleName) {
+        this.ruleName = ruleName;
+    }
+}
